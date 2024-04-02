d7fc5ea71364139716cd94057f85c18e1d89eaa9
Eeemlo <emma.lorensson@outlook.com>
2024-04-02 13:24:37 +0200

    Skapat interface Todo

---
+++ src/ts/main.ts
@@ -0,0 +1,25 @@
+/*Steg 1: Skapa Todo-interfacet*/
+
+interface Todo {
+    task: string,
+    completed: boolean,
+    priority: number
+};
+
+/*Steg 2: Implementera Todo-klassen */
+
+class ToDoList {
+    //Array som lagrar todo-objekt
+    todos: Todo[];
+
+    //Metod för att lägga till todos med prioritet
+
+    //Metod för att markera todos som klara
+
+    //Metod för att hämta listan av Todos
+
+    //Metod för att spara todos till LocalStorage
+
+    //Metod för att hämta todos från LocalStorage
+
+}
